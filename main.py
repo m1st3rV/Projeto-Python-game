bd94004803b000b958b6be636c3326250139426d
Danilo Venancio <danilovenanciof@gmail.com>
2026-02-23 16:04:45 -0300

    new file:   Game.py 	new file:   Menu.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import pygame
+from code.Game import Game
 
-print(f'Setup Start')
-
-pygame.init()
-window = pygame.display.set_mode(size=(400, 300))
-print(f'Setup End')
-
-print('setup start')
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Close window
-            print('setup end')
-            quit() #Close program
+game = Game()
+game.run()
